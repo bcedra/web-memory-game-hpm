8b3bb3826bd31ced04a690b375d7b391e1599504
TaniaCarina <126615527+TaniaCarina@users.noreply.github.com>
2024-07-16 11:37:09 +0300

    buttonOk

---
+++ index.js
@@ -0,0 +1,5 @@
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form from refreshing the page
+    <button onclick="document.getElementById('nb').innerHTML"></button>
+});
