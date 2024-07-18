efa27b1e35d63a4de1355ad2d95ef6070795c2e0
TaniaCarina <126615527+TaniaCarina@users.noreply.github.com>
2024-07-18 14:05:50 +0300

    task_3_tot

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
-
 const cookieKey = "cookieTest123";
-function onSubmit(){
-  document.getElementById("myForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent form from refreshing the page
-  var name = document.getElementById("username").value;
-  var date = document.getElementById("date").value;
-  var element = document.getElementById("element").value;
-  console.log(name, date, element);
-  setCookie()
 
-  let object = {name: name, date: date, category:element};
+function onSubmit() {
+  document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form from refreshing the page
 
-  console.log(object);
+    clearErrors();
+    let hasError = false;
 
-  var objectSerilized = JSON.stringify(object);
-  var objectDeserilized = JSON.parse(objectSerilized);
+    var name = document.getElementById("username").value;
+    var date = document.getElementById("date").value;
+    var age = calculateAge(new Date(date));
+    var element = document.getElementById("element").value;
+    console.log(name, date, element, age);
 
-  setCookie(cookieKey, objectSerilized, 30);
+    if (!name) {
+      setError("username", "Acest camp este obligatoriu!");
+      hasError = true;
+    }
 
-});
+    if (!date) {
+      setError("date", "Acest camp este obligatoriu!");
+      hasError = true;
+    }
+
+    if (!element) {
+      setError("element", "Acest camp este obligatoriu!");
+      hasError = true;
+    }
+
+    if (hasError) {
+      return;
+    }
+
+    let object = { name: name, date: date, category: element, age: age };
+
+    console.log(object);
+
+    var objectSerilized = JSON.stringify(object);
+
+    setCookie(cookieKey, objectSerilized, 30);
+  });
 }
 
-function checkDate() {
-  const today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth() + 1).padStart(2, '0'); 
-  const yyyy = today.getFullYear();
-
-  const formattedDate = yyyy + '/' + mm + '/' + dd;
-
-  return formattedDate;
+function setError(fieldId, errorMessage) {
+  const field = document.getElementById(fieldId);
+  const errorField = document.createElement("div");
+  errorField.className = "error";
+  errorField.innerText = errorMessage;
+  field.parentNode.appendChild(errorField);
 }
 
-function setCookie(cname,cvalue,exdays) {
+function clearErrors() {
+  const errorFields = document.querySelectorAll(".error");
+  errorFields.forEach(function (errorField) {
+    errorField.remove();
+  });
+}
+
+function setCookie(cname, cvalue, exdays) {
   const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   let expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
@@ -43,7 +68,7 @@ function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
   let ca = decodedCookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
@@ -55,31 +80,44 @@ function getCookie(cname) {
   return "";
 }
 
-function validateForm() {
-  let x = document.forms["myForm"]["username"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
+function loadCookie() {
+  let cookieValue = getCookie(cookieKey);
+  if (cookieValue) {
+    let objectDeserilized = JSON.parse(cookieValue);
+    document.getElementById("username").value = objectDeserilized.name;
+    document.getElementById("date").value = objectDeserilized.date;
+    document.getElementById("element").value = objectDeserilized.category;
   }
 }
 
-function loadCookie() {
-  let cookieValue = getCookie(cookieKey);
-  if(cookieValue) {
-      let objectDeserilized = JSON.parse(cookieValue);
-      document.getElementById("username").value=objectDeserilized.name;
-      document.getElementById("date").value=objectDeserilized.date;
-      document.getElementById("element").value=objectDeserilized.category;
-  }
-  }
-window.onload=loadCookie;
+function checkDate() {
+  const dateInput = document.getElementById('date');
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); //ianuarie = 0
+  const dd = String(today.getDate()).padStart(2, '0'); //completeaza cu 0 daca sirul are un caracter(<2)
+  const formattedToday = `${yyyy}-${mm}-${dd}`;
 
+  dateInput.setAttribute('max', formattedToday);
+}
 
-function main(){
+function calculateAge(birthDate) {
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
 
-  onSubmit();
-  loadCookie();
+  if (today.getMonth() < birthDate.getMonth() ||
+    (today.getMonth() == birthDate.getMonth() && today.getDay() < birthDate.getDay()))
+    age--;
 
+  return age;
+}
+
+function main() {
+  document.addEventListener('DOMContentLoaded', function () {  //toate elem. html au fost create si accesibile prin DOM
+    checkDate();
+    loadCookie();
+    onSubmit();
+  });
 }
 
 main();
