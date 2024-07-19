514cd506acee25e08c279cf741f473ce33863f11
TaniaCarina <126615527+TaniaCarina@users.noreply.github.com>
2024-07-19 16:40:14 +0300

    randPairs

--- index.js
+++ index.js
@@ -1,68 +1,10 @@
-
-
-var tableData = [];
-
-ImageArray = new Array();
-image[0] = 'Images/1.jpeg';
-image[1] = 'Images/2.jpeg';
-image[2] = 'Images/3.jpeg';
-image[3] = 'Images/4.jpeg';
-image[4] = 'Images/5.jpeg';
-
-
 var imgAr = ["Images/1.jpeg", "Images/2.jpeg", "Images/3.jpeg", "Images/4.jpeg", "Images/5.jpeg"];
 function imgRandom(imgArr) {
   return imgArr[Math.floor(Math.random() * imgArr.length)];
 }
 
 
-function onSubmit() {
-  document.getElementById("myForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    var table = document.getElementById("myTable");
-    table.innerHTML = "";
-
-    var sizeInput = document.getElementById("nb");
-    var sizee = parseInt(sizeInput.value);
-
-    for (var i = 0; i < sizee; i++) {
-      var rowData = [];
-      var row = document.createElement("tr");
-
-      for (var j = 0; j < sizee; j++) {
-        var cell = document.createElement("td");
-        const textnoder = document.createTextNode(i);
-        const textnodec = document.createTextNode(j);
-
-        var img = document.createElement('img');
-        img.src = "Images/1.jpeg";
-
-        //   var rand = ImageArray[Math.floor(Math.random() * ImageArray.length)];
-        //   var img = new Image();
-        //   img.src = rand;
-
-
-        img.setAttribute("width", "100");
-        img.setAttribute("height", "100");
-        cell.appendChild(img);
-
-        row.appendChild(cell);
-        cell.append(textnoder);
-        cell.append(textnodec);
-      }
-      table.appendChild(row);
-      tableData.push(rowData);
-    }
-  });
-}
-
-function main() {
-  onSubmit();
-}
-
-main();
-
+var tableData = [];
 
 function createTable() {
 
@@ -87,18 +29,12 @@ function createTable() {
 
     for (var j = 0; j < sizee; j++) {
       var cell = document.createElement("td");
-      //const textnoder = document.createTextNode(i);
-      //const textnodec = document.createTextNode(j);
+      
       var number = valCels.pop();
       const textnode = document.createTextNode(number);
 
       var img = document.createElement('img');
       img.src = "Images/1.jpeg";
-
-      //   var rand = ImageArray[Math.floor(Math.random() * ImageArray.length)];
-      //   var img = new Image();
-      //   img.src = rand;
-
 
       img.setAttribute("width", "100");
       img.setAttribute("height", "100");
@@ -106,7 +42,7 @@ function createTable() {
 
       row.appendChild(cell);
       cell.append(textnode);
-      //cell.append(textnodec);
+
     }
     table.appendChild(row);
     tableData.push(rowData);
