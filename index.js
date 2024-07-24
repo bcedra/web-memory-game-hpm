6804a0f0b791ae1a26d15b8f5e512f6127f4c4b2
LAPTOP-QKLEBE98\iulia <iulia.hanganu67@yahoo.com>
2024-07-24 15:42:08 +0300

    task4_Iulia

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ document.getElementById("submit").addEventListener("click", function(event) {
     var table=document.createElement("table");
     table.id="myTable";
 
+    var cellNumber = row * row; 
 
+    var cellValues = [];  
+    for (var i = 1; i <= cellNumber / 2; i++) {        
+        cellValues.push(i, i);
+    }
+
+    cellValues = shuffle(cellValues);
+
+    
     for(var ri=0; ri<row; ri++) {
         var tr=document.createElement("tr");
          for(var ci=0; ci<col; ci++){
             var td=document.createElement("td");
+
+            var number = cellValues.pop();
             
             var flipCard = document.createElement("div");
             flipCard.className = "flipCard";
@@ -32,7 +43,7 @@ document.getElementById("submit").addEventListener("click", function(event) {
 
             var flipCardBack = document.createElement("div");
             flipCardBack.className = "flipCard-back";
-            flipCardBack.textContent = Math.floor(Math.random() * document.getElementById("number").value);
+            flipCardBack.textContent = `${number}`;
 
             flipCardInner.appendChild(flipCardFront);
             flipCardInner.appendChild(flipCardBack);
@@ -46,3 +57,12 @@ document.getElementById("submit").addEventListener("click", function(event) {
     container.appendChild(table);
 });
 
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp; 
+    }
+    return array;
+}
