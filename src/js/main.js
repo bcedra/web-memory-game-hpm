63decf5adce9c81642bf3c5f82c16a4e42608e5c
TaniaCarina <126615527+TaniaCarina@users.noreply.github.com>
2024-08-07 11:35:32 +0300

    task_13_refacut
    Plus timer si buton de replay ce reseteaza timer-ul.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 import { initializeTranslations } from './translations.js';
+import { addStartListeners } from './game.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 	initializeTranslations();
+
+	addStartListeners();
 
 	console.log('Hello World!');
 });
