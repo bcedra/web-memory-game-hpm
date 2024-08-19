135a9061ddc4b78353b80dd773b9f9691288e024
Bogdan Popa <bogdanpopacedra@yahoo.com>
2024-08-19 12:13:25 +0300

    [36] - Fix conflicts

--- api/index.js
+++ api/index.js
@@ -62,11 +62,11 @@ async function main() {
 
 	app.post('/leaderboard', async (req, res) => {
 		const { username, difficulty, time_in_seconds, attempts } = req.body;
-		
+
 		// check if all fields have a value
 		if (!username || !difficulty || time_in_seconds === undefined || attempts === undefined) {
 			return res.status(400).json({ error: 'All fields are required' });
-		} 
+		}
 
 		try {
 			//  insert data into table
@@ -85,8 +85,6 @@ async function main() {
 		const [n] = await pool.query('SELECT * FROM leaderboard');
 		res.json(n);
 	});
-
-	app.post('/leaderboard', async (req, res) => {});
 }
 
 main();
